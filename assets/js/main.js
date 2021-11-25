8af25d04789989a663b4467e3b65804b167f22c8
MeeranB <v9@live.co.uk>
2021-11-25 11:02:55 +0000

    Install babel and make sidebar IE compatible

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,88 @@
+"use strict";
+
 // Fix sidebar styling on smaller viewport widths.
 // Sidebar menu has hover options and is shorter
 // Fix issue with project cards appearing over sidebar
+console.log("test");
+var menuBtn = $(".menu-btn");
+var sideBar = $(".sidebar");
+var main = $("main"); //Initialise sidebar animation
 
-const menuBtn = $(".menu-btn");
-const sideBar = $(".sidebar");
-const main = $("main");
-
-//Initialise sidebar animation
 sideBar.sidebar({
-    side: "left",
-    close: false,
+  side: "left",
+  close: false
 });
 
-window.onload = () => {
-    //Display menu button if jquery is available
-    if ($ === jQuery) {
-        if (!window.matchMedia("(min-width: 768px)").matches) {
-            $(sideBar).trigger("sidebar:close", [{ speed: 0 }]);
-            menuBtn.css("display", "flex");
-            main.css("margin-left", 0);
-        } else if (!window.matchMedia("(min-width: 992px)").matches) {
-            main.css("margin-left", "200px");
-        } else if (!window.matchMedia("(min-width: 992px)").matches) {
-            main.css("margin-left", "250px");
-        }
+window.onload = function () {
+  //Display menu button if jquery is available
+  if ($ === jQuery) {
+    if (!window.matchMedia("(min-width: 768px)").matches) {
+      $(sideBar).trigger("sidebar:close", [{
+        speed: 0
+      }]);
+      menuBtn.css("display", "flex");
+      main.css("margin-left", 0);
+    } else if (!window.matchMedia("(min-width: 992px)").matches) {
+      main.css("margin-left", "200px");
+    } else if (!window.matchMedia("(min-width: 992px)").matches) {
+      main.css("margin-left", "250px");
     }
+  }
 };
 
-let windowSize = $(window).width();
+var windowSize = $(window).width();
 
 function checkWidth() {
-    windowSize = $(window).width();
-    return windowSize;
+  windowSize = $(window).width();
+  return windowSize;
 }
 
-$(window).on("resize", () => {
-    checkWidth();
-    if (windowSize > 992) {
-        menuBtn.hide();
-        $(sideBar).trigger("sidebar:open", [{ speed: 0 }]);
-        sideBar.off("sidebar:opened", closeSideBarOnMainClick);
-        main.css("margin-left", "250px");
-    } else if (windowSize > 767) {
-        menuBtn.hide();
-        $(sideBar).trigger("sidebar:open", [{ speed: 0 }]);
-        sideBar.off("sidebar:opened", closeSideBarOnMainClick);
-        main.css("margin-left", "200px");
-    } else if (windowSize <= 767) {
-        menuBtn.css("display", "flex");
-        sideBar.trigger("sidebar:close", [{ speed: 0 }]);
-        main.css("margin-left", 0);
-    }
-});
+$(window).on("resize", function () {
+  checkWidth();
 
-$(menuBtn).on("click", () => {
+  if (windowSize > 992) {
     menuBtn.hide();
-    sideBar.trigger("sidebar:open");
-});
-
-sideBar.on("sidebar:closed", () => {
+    $(sideBar).trigger("sidebar:open", [{
+      speed: 0
+    }]);
+    sideBar.off("sidebar:opened", closeSideBarOnMainClick);
+    main.css("margin-left", "250px");
+  } else if (windowSize > 767) {
+    menuBtn.hide();
+    $(sideBar).trigger("sidebar:open", [{
+      speed: 0
+    }]);
+    sideBar.off("sidebar:opened", closeSideBarOnMainClick);
+    main.css("margin-left", "200px");
+  } else if (windowSize <= 767) {
     menuBtn.css("display", "flex");
+    sideBar.trigger("sidebar:close", [{
+      speed: 0
+    }]);
+    main.css("margin-left", 0);
+  }
 });
-
+$(menuBtn).on("click", function () {
+  menuBtn.hide();
+  sideBar.trigger("sidebar:open");
+});
+sideBar.on("sidebar:closed", function () {
+  menuBtn.css("display", "flex");
+});
 sideBar.on("sidebar:opened", closeSideBarOnMainClick);
 
 function closeSideBarOnMainClick() {
-    $("main:not(nav)").on("click", () => {
-        if (windowSize <= 767) {
-            $(sideBar).trigger("sidebar:close");
-        }
-    });
+  $("main:not(nav)").on("click", function () {
+    if (windowSize <= 767) {
+      $(sideBar).trigger("sidebar:close");
+    }
+  });
 }
 
-const heroSubtitle = $(".hero-text--subtitle");
-
-const typewriter = new Typewriter(heroSubtitle[0], {
-    loop: true,
-    delay: 75,
+var heroSubtitle = $(".hero-text--subtitle");
+var typewriter = new Typewriter(heroSubtitle[0], {
+  loop: true,
+  delay: 75
 });
-
-typewriter
-    .pauseFor(1000)
-    .typeString("I am a web developer")
-    .pauseFor(1500)
-    .deleteChars(13)
-    .typeString("web designer")
-    .pauseFor(1500)
-    .deleteChars(18)
-    .changeDelay(60)
-    .typeString(
-        " could be an invaluable asset to <strong> your </strong> business!"
-    )
-    .pauseFor(2500)
-    .start();
-
+typewriter.pauseFor(1000).typeString("I am a web developer").pauseFor(1500).deleteChars(13).typeString("web designer").pauseFor(1500).deleteChars(18).changeDelay(60).typeString(" could be an invaluable asset to <strong> your </strong> business!").pauseFor(2500).start();
 $("#contact-form").validate();
